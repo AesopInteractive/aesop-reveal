df2485d9d449fb701f58ea3bee7247b53358902f
hyunster <hyunsupul@yahoo.com>
2018-08-29 16:45:15 -0500

    1.2.1

--- block/reveal/block.js
+++ block/reveal/block.js
@@ -24,7 +24,7 @@
 	registerBlockType( 'ase/reveal', { // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
 		title: __( 'Aesop Reveal Block', 'ASE' ), // Block title.
 		icon: 'image-flip-horizontal', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-		category: 'widgets', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+		category: 'aesop-story-engine', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 		
 		attributes: {
 			width : {
